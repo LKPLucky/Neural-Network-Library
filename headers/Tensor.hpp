286828c536563066c8e5c120f37c5a19ca946c80
LKPLucky <lakshperchani2005@gmail.com>
2025-05-10 04:25:36 +0500

    Made Tensor Struct to use as Data throughout the Project

---
+++ headers/Tensor.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct Tensor
+{
+    vector<double> Data;
+    int Dim1; // Cols
+    int Dim2; // Rows
+    int Dim3; // Channels
+    int Dim4; // Filter
+
+    // Constructors
+    Tensor() = default;
+    Tensor(int D1, int D2 = 1, int D3 = 1, int D4 = 1);
+    Tensor(const Tensor& other); // Copy Constructor (NOT USED)
+  
+    // Overload Operator () to access Data using indexes
+    double& operator()(int x, int y = 0, int z = 0, int f = 0);
+
+    void Display() const;
+    
+};
---
+++ src/Tensor.cpp
@@ -0,0 +1,20 @@
+#include "..\headers\Tensor.hpp"
+
+// Constructor
+Tensor::Tensor(int D1, int D2, int D3, int D4) : Dim1(D1), Dim2(D2), Dim3(D3), Dim4(D4) 
+{
+    Data.resize(Dim1 * Dim2 * Dim3 * Dim4);
+}
+
+// Copy Constructor (NOT USED)
+Tensor::Tensor(const Tensor& other) : Data(other.Data), Dim1(other.Dim1), Dim2(other.Dim2), Dim3(other.Dim3), Dim4(other.Dim4) {}
+
+
+// Overload Operator () to access Data using indexes
+double& Tensor::operator()(int x, int y, int z, int f)
+{
+    return Data[(f * Dim1 * Dim2 * Dim3) + (z * Dim1 * Dim2) + (y * Dim1) + x];
+}
+
+
+
